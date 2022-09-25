b75bbf62ecaf936781d164aa9b104f474e63ed2d
Marc <marc@unit37.org>
2022-09-25 09:59:46 +0300

    add search field and button (no function)

--- App.js
+++ App.js
@@ -1,8 +1,20 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 import MapView, {Marker} from 'react-native-maps';
-import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import { StyleSheet, Text, View, Dimensions, TextInput, Alert, StatusBar, Button } from 'react-native';
 
 export default function App() {
+
+  const [address, setAddress] = useState('');
+
+  const getLocation = () => {
+    fetch(`example.com/find?i=${keyword}`)
+    .then(response => response.json())
+    .then(responseJson => setLocation(responseJson))
+    .catch(error => { 
+        Alert.alert('Error', error); 
+    });    
+  }
+
   return (
     <View style={styles.container}>
       <MapView style={styles.map} 
@@ -10,6 +22,10 @@ export default function App() {
       >
         <Marker coordinate={{latitude:60.201373, longitude:24.934041}}title='Haaga-Helia' />
       </MapView>
+
+      <TextInput style={styles.textInput} placeholder='address' 
+        onChangeText={text => setAddress(text)} />
+      <Button title="Find" onPress={getLocation} />
     </View>
   );
 }
@@ -22,6 +38,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   map: {
+    flex: 1,
     width: Dimensions.get('window').width,
     height: Dimensions.get('window').height,
   },
